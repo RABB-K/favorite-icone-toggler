be5a8a1cf1308e2786691ddefcc4aca9bbbc95d7
Rabb-k <rabbinkambere@gmail.com>
2025-06-04 15:34:15 +0200

    create basic features with html, css and javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const icons = document.querySelectorAll('.favorite-icon');
+
+icons.forEach(button=>{
+  button.addEventListener('click',()=>{
+    if(button.classList.contains('filled')){
+      button.classList.remove('filled');
+      button.innerHTML = "&#9825"; // Empty heart
+    }
+    else{ button.classList.add('filled');
+    button.innerHTML = "&#10084";} // filled Heart
+  });
+});
